4db2e047d62f00e5668e5835b27b3fc54c0d9185
William Condori Quispe <williamccondori@gmail.com>
2018-07-12 14:29:37 -0500

    Se agrega componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
